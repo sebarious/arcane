4c1b2d2143562099d751d02b6ea008d6bc3f10d9
Sebarious <matthewdixon@denfield.co.uk>
2026-09-04 09:51:49 +0100

    chore: wip

--- app/Services/Banding/RarityBander.php
+++ app/Services/Banding/RarityBander.php
@@ -12,14 +12,14 @@ class RarityBander
      *   rare:      £4.50 – £10.49
      *   super:     £10.50 – £49.99
      *   legendary: £50.00 – £149.99
-     *   mythic:    £150.00 – £399.99
+     *   mythic:    £150.00 – £349.99
      */
     public const DEFAULT_THRESHOLDS = [
         'common'    => ['min' => 75,    'max' => 449],
         'rare'      => ['min' => 450,   'max' => 1049],
         'super'     => ['min' => 1050,  'max' => 4999],
         'legendary' => ['min' => 5000,  'max' => 14999],
-        'mythic'    => ['min' => 15000,  'max' => 39999],
+        'mythic'    => ['min' => 15000,  'max' => 34999],
     ];
 
     /** @param array<string, array{min:int,max:int}>|null $thresholds */
