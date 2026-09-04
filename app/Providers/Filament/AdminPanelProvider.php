8623ba42ce2012a0dacb4fb23e38aa65030d9838
Sebarious <matthewdixon@denfield.co.uk>
2026-09-04 17:55:36 +0100

    chore: wip

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -56,8 +56,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                DashboardStats::class,
                 TheoreticalBatchesWidget::class,
+                DashboardStats::class,
                 CancelledBatchesWidget::class,
                 AttentionList::class,
                 MarginRealisedWidget::class,
